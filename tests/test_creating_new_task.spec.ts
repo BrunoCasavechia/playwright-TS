88a3f1f34ee8916ad4ddfb1c919147b9337b5201
Bruno Casavechia <bruno.casavechia@fit-tecnologia.org.br>
2026-02-10 16:20:36 -0300

    Testing using the API Helper

--- tests/test_creating_new_task.spec.ts
+++ tests/test_creating_new_task.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('User should be able to add a new task', async ({ page }) => {
+test('User should be able to add a new task', async ({ page, request }) => {
+
+  const taskName = 'Writing a new task here'
+
+  await request.delete('http://localhost:3333/helper/tasks/' + taskName) 
+
   await page.goto('http://localhost:8080');
 
   const inputTaskName = page.locator('input[class*=InputNewTask]')
-  await inputTaskName.fill('Writing a new task here')
+  await inputTaskName.fill(taskName)
 
   await page.click('css = button >> text = Create')
-});
+}); 
